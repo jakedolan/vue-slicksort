781a8ba6a2f1d18426ff0193ff2e731bc6ec9be8
Jake <jake@cascadin.com>
2022-05-25 18:17:33 -0600

    Adding compat.mode = 3

--- src/components/SlickItem.ts
+++ src/components/SlickItem.ts
@@ -4,6 +4,9 @@ import { ElementMixin } from '../ElementMixin';
 export const SlickItem = defineComponent({
   name: 'SlickItem',
   mixins: [ElementMixin],
+  compatConfig: {
+    MODE: 3,
+  },
   props: {
     tag: {
       type: String,
